6a868de9a43e20458388ef794731d9a3e28d3a93
Anil Yelam <ayelam@vmware.com>
2022-04-25 12:31:19 -0700

    fix for linux api change https://github.com/torvalds/linux/commit/ba1e78a1dc0ca3e92f0be82279e6ba24177af7d6

--- shenango/ksched/ksched.c
+++ shenango/ksched/ksched.c
@@ -541,7 +541,7 @@ static int __init ksched_cpuidle_hijack(void)
 	drv = cpuidle_get_driver();
 	if (!drv)
 		return -ENOENT;
-	if (drv->state_count <= 0 || drv->states[0].disabled)
+	if (drv->state_count <= 0 || drv->states[0].flags & CPUIDLE_FLAG_UNUSABLE)
 		return -EINVAL;
 
 	cpuidle_pause_and_lock();
